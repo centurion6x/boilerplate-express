c1eded05c17afa504c17e646c15e2b1f8f73ca2d
Gamunu Bandara <gamunu99sb@gmail.com>
2022-05-31 18:08:34 +0530

    serving an html file

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
-
+//console.log("Hello World");
+absPathIndex = __dirname + "/views/index.html" ;
 
 app.get("/", (req, res) => {
-    res.send("Hello Express")
-} )
+    res.sendFile(absPathIndex) ;
+} ) ;
 
 
 
