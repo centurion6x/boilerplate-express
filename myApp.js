3fe28d02ad65718f8dc70205e5e4aa069d1825c6
Gamunu Bandara <gamunu99sb@gmail.com>
2022-05-31 18:46:06 +0530

    add filehandler middleware

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ let app = express();
 
 //console.log("Hello World");
 absPathIndex = __dirname + "/views/index.html" ;
-
+absPathPublic = __dirname + "/public" ;
+const fileServerMiddle = express.static(absPathPublic) ;
+app.use("/public",absPathIndex) ;
 app.get("/", (req, res) => {
     res.sendFile(absPathIndex) ;
 } ) ;
