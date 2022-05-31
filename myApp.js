bb91311a599bdb2eda101fc241cdeac6e54ec0ee
Gamunu Bandara <gamunu99sb@gmail.com>
2022-05-31 19:06:58 +0530

    jason api

--- myApp.js
+++ myApp.js
@@ -2,13 +2,22 @@ let express = require('express');
 let app = express();
 
 //console.log("Hello World");
-absPathIndex = __dirname + "/views/index.html" ;
-absPathPublic = __dirname + "/public" ;
+const absPathIndex = __dirname + "/views/index.html" ;
+const absPathPublic = __dirname + "/public" ;
 const fileServerMiddle = express.static(absPathPublic) ;
+
 app.use("/public",fileServerMiddle) ;
+
 app.get("/", (req, res) => {
     res.sendFile(absPathIndex) ;
 } ) ;
+
+app.get("/json", (req,res) => {
+    res.json({
+        "message" : "Hello JASON"
+    })
+}) ;
+
 
 
 
